fc9ed1232497d77a41175f0fbf91fcdcac88a1c0
Pushpendre Rastogi <pushpendre@gmail.com>
2015-03-24 22:30:05 -0400

    Fix the ls dired error on mac

--- pushpendrerastogi_custom_mac.el
+++ pushpendrerastogi_custom_mac.el
@@ -11,6 +11,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(dired-use-ls-dired nil)
  '(ecb-options-version "2.40")
  '(flycheck-checkers
    (quote
@@ -67,4 +68,3 @@
  '(semantic-decoration-on-unparsed-includes ((t nil)))
  '(semantic-highlight-func-current-tag-face ((t (:background "white"))))
  '(semantic-tag-boundary-face ((t nil))))
-
